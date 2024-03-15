4ff0bb45d0780586805fa122c8a7c15587abd113
MichaelYungLee <mikenikey@g.ucla.edu>
2024-03-14 19:56:49 -0700

    Removed unnecessary Mac folder

--- cdk/typescript-workshop/lib/pipeline-stack.ts
+++ cdk/typescript-workshop/lib/pipeline-stack.ts
@@ -1,14 +1,30 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Repository } from 'aws-cdk-lib/aws-codecommit';
+import { CodeBuildStep, CodePipelineSource, CodePipeline } from 'aws-cdk-lib/pipelines';
 
 export class WorkshopPipelineStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
         
         // Pipeline code goes here
-        new Repository(this, 'WorkshopRepo', {
+        const repo = new Repository(this, 'WorkshopRepo', {
             repositoryName: "WorkshopRepo"
+        });
+
+        const pipeline = new CodePipeline(this, 'Pipeline', {
+            pipelineName: 'WorkshopPipeline',
+            synth: new CodeBuildStep('SynthStep', {
+                input: CodePipelineSource.codeCommit(repo, 'main'),
+                installCommands: [
+                    'npm install -g aws-cdk'
+                ],
+                commands: [
+                    'npm ci',
+                    'npm run build',
+                    'npx cdk synth'
+                ]
+            })
         });
     }
 }
